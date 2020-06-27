7ca9724fe4fd3b5e299b85a653209739cf7365b8
ykawamura96 <y-kawamura@jsk.imi.i.u-tokyo.ac.jp>
2020-06-27 12:51:14 +0900

    add plot options!

--- software/00_plot.py
+++ software/00_plot.py
@@ -1,6 +1,5 @@
 import argparse
 from matplotlib import pyplot as plt
-
 import pickle
 from datetime import datetime
 from collections import namedtuple
@@ -8,14 +7,14 @@ from collections import namedtuple
 MeasuredData = namedtuple('MeasuredData',
                           ['os', 'title', 'x_data', 'y_data', 'x_label', 'y_label'])
 
-
-def plot(m):
-    x = m.x_data
-    ave = m.y_data[0]
-    std = m.y_data[1]
+def plot(ms):
     fig = plt.figure()
-    plt.plot(x, ave)
-    plt.fill_between(x, ave-std, ave+std, alpha=0.5)
+    for m in ms:
+        x = m.x_data
+        ave = m.y_data[0]
+        std = m.y_data[1]
+        plt.plot(x, ave)
+        plt.fill_between(x, ave-std, ave+std, alpha=0.5)
     plt.title(m.title)
     plt.xlabel(m.x_label)
     plt.ylabel(m.y_label)
@@ -24,10 +23,10 @@ def plot(m):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot results.')
-    parser.add_argument("-i", dest="filename", required=True,
+    parser.add_argument("-i", dest="filenames", required=True, nargs='+',
                         help="input file with two matrices", metavar="FILE")
     args = parser.parse_args()
-    path = args.filename
-    with open(path, 'r') as f:
-        m = pickle.load(f)
-        plot(m)
+    files = args.filenames
+    ms = [pickle.load(open(f, 'rb')) for f in files]
+    plot(ms)
+    plt.show()
